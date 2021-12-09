36140bd0d9ab9ab7e708d4cb920a33ce08794e17
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-12-09 08:39:58 +0100

    Untrack content

--- site/config/config.php
+++ site/config/config.php
@@ -8,25 +8,30 @@
  * All config options: https://getkirby.com/docs/reference/system/options
  */
 return [
-    'debug' => false,
+  'debug' => false,
 
-    // Markdown plugin
-    'community.markdown-field.buttons'    => [['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], 'bold', 'italic', 'divider', 'link','pagelink', 'email', 'file', 'divider', 'ul', 'ol', 'blockquote'],
-    'community.markdown-field.font'       => [
-        'family'  => 'sans-serif',
-        'scaling' => true,
-        'size'    => 'regular',
-    ],
-    'community.markdown-field.query'      => [
-        'pagelink' => null,
-        'images'   => 'page.images',
-        'files'    => 'page.files.filterBy("type", "!=", "image")',
-    ],
-    'community.markdown-field.modals'     => true,
-    'community.markdown-field.blank'      => false,
-    'community.markdown-field.invisibles' => false,
+  // Markdown plugin
+  'community.markdown-field.buttons'    => [['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], 'bold', 'italic', 'divider', 'link','pagelink', 'email', 'file', 'divider', 'ul', 'ol', 'blockquote'],
+  'community.markdown-field.font'       => [
+      'family'  => 'sans-serif',
+      'scaling' => true,
+      'size'    => 'regular',
+  ],
+  'community.markdown-field.query'      => [
+      'pagelink' => null,
+      'images'   => 'page.images',
+      'files'    => 'page.files.filterBy("type", "!=", "image")',
+  ],
+  'community.markdown-field.modals'     => true,
+  'community.markdown-field.blank'      => false,
+  'community.markdown-field.invisibles' => false,
 
-    // thumbs
+  // autoresize
+  // 'medienbaecker.autoresize.maxWidth' => 2000,
+  // 'medienbaecker.autoresize.maxHeight' => 2000,
+  // 'medienbaecker.autoresize.quality' => 90,
+
+  // thumbs
   'thumbs' => [
     'quality' => 80,
     'driver' => 'im',
